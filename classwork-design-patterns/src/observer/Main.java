71b18b3ef2b74f27421555f52be9999d436af476
irow-git <stu1801321014@uni-plovdiv.bg>
2021-04-21 18:55:44 +0300

    observer variation

--- classwork-design-patterns/src/observer/Main.java
+++ classwork-design-patterns/src/observer/Main.java
@@ -8,9 +8,11 @@ public class Main {
 
 		Observer obs1 = new Person("01", "painting");
 		Observer obs2 = new Person("02", "painting");
+		Observer obs3 = new Person("03", ";painting");
 
 		price.add(obs1);
 		price.add(obs2);
+		price.add(obs3);
 
 		price.setPrice("250lv");
 		price.setPrice("350lv");
