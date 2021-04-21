25aa00e8d13f767c40a88e1f2b194db71c26e57b
irow-git <stu1801321014@uni-plovdiv.bg>
2021-04-21 18:13:07 +0300

    observer

--- classwork-design-patterns/src/observer/Main.java
+++ classwork-design-patterns/src/observer/Main.java
@@ -9,12 +9,10 @@ public class Main {
 		Observer obs1 = new Person("01", "painting");
 		Observer obs2 = new Person("02", "painting");
 		Observer obs3 = new Person("03", "painting");
-		Observer obs4 = new Person("04", "painting");
 
 		price.add(obs1);
 		price.add(obs2);
 		price.add(obs3);
-		price.add(obs4);
 
 		price.setPrice("250lv");
 		price.setPrice("350lv");
